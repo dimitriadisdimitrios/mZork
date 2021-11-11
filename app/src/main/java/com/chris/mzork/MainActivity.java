ffae8be6c8ba6565e659da6f6c81524fa5746b06
Dimitrios Dimitriadis <dimitris.dimtriadis@gmail.com>
2021-11-11 22:03:59 +0200

    v0.2: Prevent from erase system message in editText. First approach of auto-answering of system. (Broken commit)

--- app/src/main/java/com/chris/mzork/MainActivity.java
+++ app/src/main/java/com/chris/mzork/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 
 import com.chris.mzork.databinding.ActivityMainBinding;
@@ -13,9 +14,10 @@ import org.apache.commons.lang3.StringUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding bind;
-    String mainText;
-
+    private ActivityMainBinding bind;
+    private String mainText;
+    private Boolean enableToErase;
+    private Boolean systemNeedToAnswer = true; // Global private variable to trigger the times that need system to answer
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,25 +29,45 @@ public class MainActivity extends AppCompatActivity {
         bind.etTextEditor.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Save text before changing
                 mainText = s.toString();
+                // Check if last character in text is '\n'
+                enableToErase = !mainText.substring(mainText.length() - 1).equals("\n");
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                checkIfAnswerIsValid(s.toString());
+                System.out.println("");
+                Log.i("MainActivity", "");
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                System.out.println("");
+                // Call function to check if need to block erasing
+                checkToBlockErase(s.toString());
+
+                if(s.toString().length() > mainText.length()){
+                    String lastCharOfNewText = s.toString().substring(s.toString().length() -1);
+                    if (lastCharOfNewText.equals("\n") && systemNeedToAnswer){
+                        systemNeedToAnswer = false;
+                        //TODO: The answers must be added dynamically
+                        bind.etTextEditor.setText("Add a new answer bellow:\n");
+                    } else {
+                        systemNeedToAnswer = true;
+                    }
+                }
             }
         });
     }
 
-    // Function that get an answer and return a valid text
-    private String checkIfAnswerIsValid(String answer){
-        // If diffText = "", we need to test if the last char from the saved text is \n
-        String diffText = StringUtils.difference(mainText, answer);
-        return "";
+    // Function that check if user erase
+    private void checkToBlockErase(String textToCheck){
+        // Compare length of text before and after of changing to see if user is able to delete a character
+        if (textToCheck.length() < mainText.length() && !enableToErase) {
+            // Set editText with text before erasing
+            bind.etTextEditor.setText(mainText);
+            // Set the position of cursor to be always at the end of the last line
+            bind.etTextEditor.setSelection(mainText.length()+1);
+        }
     }
 }
